4a7176969796165af9d49d05e111bc08b995ed7c
Avocado S <tonewday@gmail.com>
2016-09-02 00:15:29 -0700

    change port

--- src/main/scala/slack/ttt/config/Configuration.scala
+++ src/main/scala/slack/ttt/config/Configuration.scala
@@ -3,12 +3,14 @@ package slack.ttt.config
 /**
   * Created by Zhengyi on 9/1/2016.
   */
+import scala.util.Properties
+
 import com.typesafe.config.ConfigFactory
+
 import util.Try
 
 trait Configuration {
   val config = ConfigFactory.load()
-
+  lazy val servicePort = Properties.envOrElse("PORT", "80")
   lazy val serviceHost = Try(config.getString("service.host")).getOrElse("localhost")
-  lazy val servicePort = Try(config.getInt("service.port")).getOrElse(8080)
 }
